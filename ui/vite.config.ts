441da7b95f73497363f6c4a5bf6852a0bc1a4cef
DontFreakOut <dontfreakout@DontFreakOuts-MacBook-Pro.local>
2025-05-11 07:36:45 -0400

    Deploy to github pages

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/digital-portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
