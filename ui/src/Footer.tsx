b5b69cf349588310d73bb4d8be834d7c45042933
DontFreakOut <dontfreakout@DontFreakOuts-MacBook-Pro.local>
2025-05-13 10:13:31 -0400

    Added Contact form and resume download button

--- ui/src/Footer.tsx
+++ ui/src/Footer.tsx
@@ -1,8 +1,28 @@
 import './App.css'
 
-const Footer = () => {
+type FooterProps = {
+  theme?: string;
+}
+
+const Footer = ({theme='white'}: FooterProps) => {
+
+  const pathToResume = '/digital-portfolio/resume.pdf';
+
   return (
-    <p>Footer</p>
+    <footer className={`large-menu ${theme}`}>
+      <p>&copy; 2025 Kay Rubio</p>
+      <div className='large-menu-project-button-group'>
+        <a href={pathToResume} download='resume.pdf'>
+          Resume
+        </a>
+        <a href='https://www.linkedin.com/in/kay-rubio-731abb114'>
+          LinkedIn
+        </a>
+        <a href='https://github.com/KaySRubio'>
+          Github
+        </a>
+      </div>
+    </footer>
   )
 }
 
--- ui/src/Home.tsx
+++ ui/src/Home.tsx
@@ -1,16 +1,12 @@
-import { Link } from 'react-router-dom';
-import ProjectCard from './ProjectCard'
+
+import FeaturedProjects from './FeaturedProjects';
+import Contact from './Contact';
 import avatar from '@/assets/svg/avatar.svg';
 import long_wave from '@/assets/svg/long_wave.png'
 import './App.css'
-import { homePageData, projects, topics, base_path } from './data/portfolioData';
+import { homePageData } from './data/portfolioData';
 
 const Home = () => {
-
-  // find only the featured projects from the projectList
-  const featured_projects = projects
-  .filter(project => project.featured); // filter only featured
-
 
   return (
     <div>
@@ -30,26 +26,8 @@ const Home = () => {
         className="wave-background"
         style={{ backgroundImage: `url(${long_wave})` }}
       ></div>
-      <div className="featured-projects-section">
-        <h2>Featured Projects</h2>
-        <div className='project-card-container'>
-          {featured_projects.map(project => ( <ProjectCard key={project.path} project={project} theme='white' />))}
-        </div>
-      </div>
-      <div className="featured-projects-section">
-        <h2>See more projects</h2>
-        <div className='project-icon-row'>
-          {topics.map(topic => {
-          const Icon = topic.icon; // move this outside of JSX
-          return (
-            <Link to={`${base_path}${topic.path}`} key={topic.title} className={`large-menu-button white`}>
-              <Icon className={`large-menu-icon white`} />
-              See more {topic.title}
-            </Link>
-          );
-        })}
-        </div>
-      </div>
+      <FeaturedProjects />
+      <Contact />
     </div>
     
   )
