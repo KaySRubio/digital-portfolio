e2a31f78e9f69c70011c1fae1c6649e93724f85b
DontFreakOut <dontfreakout@DontFreakOuts-MacBook-Pro.local>
2026-01-19 16:18:35 -0500

    added error handling in case cant find supabase client

--- ui/src/components/vocab/Notecards.tsx
+++ ui/src/components/vocab/Notecards.tsx
@@ -9,12 +9,29 @@ import { createClient } from "@supabase/supabase-js";
 import { fetchData, logout } from '../../utils/supabaseRequests';
 import type { Database } from '../../../database.types.ts';
 import type { Session } from "@supabase/supabase-js";
+import type { SupabaseClient } from "@supabase/supabase-js";
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase env vars missing");
+}
+
+let supabaseAvailable = false;
+let supabase: SupabaseClient | null = null;
 // Initialize database here works best
-const supabase = createClient<Database>(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
-);
+try {
+  supabase = createClient<Database>(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
+  );
+  supabaseAvailable = true;
+} catch (error) {
+  supabaseAvailable = false;
+  console.warn(error);
+}
+
 
 const Notecards = () => {
   const [email, setEmail] = useState<string>('');
@@ -30,15 +47,17 @@ const Notecards = () => {
   }, [session])
 
   async function getAndFormatWords() {
-    const wordsArray: WordData[] | undefined = await fetchData(supabase, 'spanish_vocab');
-    if(wordsArray) {
-      const wordsArrayWithSideToShow = wordsArray.map(word => {
-        return { 
-          ...word,
-          showSpanish: word.knowledgelevel === 1 ? false : true,
-        };
-      });
-      setWords(wordsArrayWithSideToShow);
+    if(supabase) {
+      const wordsArray: WordData[] | undefined = await fetchData(supabase, 'spanish_vocab');
+      if(wordsArray) {
+        const wordsArrayWithSideToShow = wordsArray.map(word => {
+          return { 
+            ...word,
+            showSpanish: word.knowledgelevel === 1 ? false : true,
+          };
+        });
+        setWords(wordsArrayWithSideToShow);
+      }
     }
   }
 
@@ -49,14 +68,17 @@ const Notecards = () => {
   }, [words])
 
   const handleLogout = () => {
-    logout(supabase);
+    if(supabase) logout(supabase);
     setSession(null);
   }
 
   return (
     <div>
       <h1>Notecards</h1>
-      { session
+      {
+        supabaseAvailable && supabase &&
+        
+        (session
         ?
         <div>
           <p>Logged in with { email }</p>
@@ -77,7 +99,9 @@ const Notecards = () => {
         </div>
         :
         <Login supabase={supabase} setEmail={setEmail} email={email} setSession={setSession} />
+        )
       }
+      
     </div>
   ) 
 }
